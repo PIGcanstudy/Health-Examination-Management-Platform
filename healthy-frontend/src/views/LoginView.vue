b01172c37ef256bd14845df0764c0ac7a38369c8
awei <2069682479@qq.com>
2024-01-08 22:17:30 +0800

    屏蔽验证码插件

--- healthy-frontend/src/views/LoginView.vue
+++ healthy-frontend/src/views/LoginView.vue
@@ -70,9 +70,10 @@ function doLogin(code) {
 function submitForm() {
   //TODO[TEST_CODE]:测试直接进入主界面
   //$router.push('/home')
+  doLogin("res.captchaVerification")
 
   // 弹出验证码框
-  useVerify('clickWord')
+  //useVerify('blockPuzzle')
 }
 
 // 验证码组件引用
@@ -96,29 +97,29 @@ function useVerify(type) {
  */
 function handleSuccess(res) {
   //TODO[TEST_CODE]:测试调用二次验证
-  Request.requestForm(
-    Request.POST,
-    '/login',
-    { captchaVerification: res.captchaVerification },
-    { baseURL: import.meta.env.VITE_CAPTCHA_URL }
-  )
-    .then((res) => {
-      console.log(res)
-      if (res.data.repCode === '0000') {
-        // 跳转到首页
-        $router.push('/home')
-        // 登录成功提示
-        ElMessage.success('登录成功，前往首页')
-        return
-      }
-      ElMessage.error('账号或密码错误')
-    })
-    .catch((res) => {
-      console.log(res)
-      ElMessage.error('账号或密码错误')
-    })
+  // Request.requestForm(
+  //   Request.POST,
+  //   '/login',
+  //   { captchaVerification: res.captchaVerification },
+  //   { baseURL: import.meta.env.VITE_CAPTCHA_URL }
+  // )
+  //   .then((res) => {
+  //     console.log(res)
+  //     if (res.data.repCode === '0000') {
+  //       // 跳转到首页
+  //       $router.push('/home')
+  //       // 登录成功提示
+  //       ElMessage.success('登录成功，前往首页')
+  //       return
+  //     }
+  //     ElMessage.error('账号或密码错误')
+  //   })
+  //   .catch((res) => {
+  //     console.log(res)
+  //     ElMessage.error('账号或密码错误')
+  //   })
 
-  //doLogin(res.captchaVerification)
+  doLogin(res.captchaVerification)
 }
 </script>
 
