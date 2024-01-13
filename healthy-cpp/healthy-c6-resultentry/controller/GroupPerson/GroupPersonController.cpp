b4cb63a32bc870f8eaa321a848b795938c723aa5
诺诺 <570951613@qq.com>
2024-01-13 19:17:14 +0800

    网页接口显示完成

--- healthy-cpp/healthy-c6-resultentry/controller/GroupPerson/GroupPersonController.cpp
+++ healthy-cpp/healthy-c6-resultentry/controller/GroupPerson/GroupPersonController.cpp
@@ -6,11 +6,11 @@ StringJsonVO::Wrapper GroupPersonController::execQueryTest(const PageQuery::Wrap
 	StringJsonVO::Wrapper vo = StringJsonVO::createShared();
 	if (pq->pageIndex == 1)
 	{
-		vo->success(String(ZH_WORDS_GETTER("test.field.succ")));
+		vo->success(String(ZH_WORDS_GETTER("qls_hep.field.succ")));
 	}
 	else
 	{
-		vo->fail(String(ZH_WORDS_GETTER("test.field.fail")));
+		vo->fail(String(ZH_WORDS_GETTER("qls_hep.field.fail")));
 	}
 	return vo;
 }
--- healthy-cpp/healthy-c6-resultentry/controller/GroupPerson/GroupPersonController.h
+++ healthy-cpp/healthy-c6-resultentry/controller/GroupPerson/GroupPersonController.h
@@ -14,14 +14,24 @@ class GroupPersonController : public oatpp::web::server::api::ApiController
 	API_ACCESS_DECLARE(GroupPersonController);
 public: // 定义接口
 	ENDPOINT_INFO(queryTest) {
-		// 定义接口描述
-		info->summary = ZH_WORDS_GETTER("test.get.summary");
-		// 定义接口响应参数类型
+		// 定义接口标题
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("qls_hep.get.summary"));
+		// 定义响应参数格式
 		API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);
-		// 定义分页参数描述
+		// 定义分页查询参数描述
 		API_DEF_ADD_PAGE_PARAMS();
-	}
-	ENDPOINT(API_M_GET, "/test", queryTest, QUERIES(QueryParams, params)) {
+		// 定义其他查询参数描述
+		API_DEF_ADD_QUERY_PARAMS(String, "name", ZH_WORDS_GETTER("qls_hep.data.name"), ZH_WORDS_GETTER("qls_hep.example.name"), false);
+		API_DEF_ADD_QUERY_PARAMS(String, "test_num", ZH_WORDS_GETTER("qls_hep.data.test_num"), ZH_WORDS_GETTER("qls_hep.example.test_num"), false);
+		API_DEF_ADD_QUERY_PARAMS(String, "dept", ZH_WORDS_GETTER("qls_hep.data.dept"), ZH_WORDS_GETTER("qls_hep.example.dept"), false);
+		API_DEF_ADD_QUERY_PARAMS(String, "physical_type", ZH_WORDS_GETTER("qls_hep.data.physical_type"), ZH_WORDS_GETTER("qls_hep.example.physical_type"), false);
+		API_DEF_ADD_QUERY_PARAMS(Int32, "is_check", ZH_WORDS_GETTER("qls_hep.data.is_check"), 0, false);
+		API_DEF_ADD_QUERY_PARAMS(Int32, "is_recheck", ZH_WORDS_GETTER("qls_hep.data.is_recheck"), 0, false);
+		API_DEF_ADD_QUERY_PARAMS(String, "start_regist_date", ZH_WORDS_GETTER("qls_hep.data.start_regist_date"), ZH_WORDS_GETTER("qls_hep.example.start_regist_date"), false);
+		API_DEF_ADD_QUERY_PARAMS(String, "finish_regist_date", ZH_WORDS_GETTER("qls_hep.data.finish_regist_date"), ZH_WORDS_GETTER("qls_hep.example.finish_regist_date"), false);
+	};
+
+	ENDPOINT(API_M_GET, "/qls_hep", queryTest, QUERIES(QueryParams, params)) {
 		// 将查询参数转换成PageQuery查询模型
 		API_HANDLER_QUERY_PARAM(pq, PageQuery, params);
 		// 执行业务逻辑处理
