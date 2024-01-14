067d6dc1df0b305c9b28db22c8a1c24a18525e1e
ChengcanGuo <2488700263@qq.com>
2024-01-14 22:28:56 +0800

    修复了bug

--- healthy-frontend/src/views/sample/search/Search.vue
+++ healthy-frontend/src/views/sample/search/Search.vue
@@ -1,55 +1,11 @@
 <template>
-  <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-form-item label="单位名称">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
-    </el-form-item>
-    <el-form-item label="信用代码">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
-    </el-form-item>
-    <el-form-item label="联系人">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
-    </el-form-item>
-    <el-form-item label="联系电话">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSearch">搜索</el-button>
-      <el-button plain @click="onReset">重置</el-button>
-    </el-form-item>
-  </el-form>
+  <div>
+    <Search></Search>
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive } from 'vue'
-
-const formInline = reactive({
-  user: '',
-  region: '',
-  date: ''
-})
-
-/**
- * ？
- */
-const onSearch = () => {
-  console.log('search!')
-}
-
-/**
- * 将表单中的输入框清空
- * ？
- */
-const onReset = () => {
-  console.log('reset!')
-}
+<script setup>
+import Search from '@/components/search/Search.vue'
 </script>
 
-<style>
-.demo-form-inline .el-input {
-  --el-input-width: 220px;
-}
-
-.demo-form-inline .el-select {
-  --el-select-width: 220px;
-}
-</style>
+<style lang="scss" scoped></style>
