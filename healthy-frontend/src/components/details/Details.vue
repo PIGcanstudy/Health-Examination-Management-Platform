e8f7a4b595a20a82b79f4381d4f8e52ec8fbc399
Vstag <starlauwin@outlook.com>
2024-01-15 19:42:39 +0800

    f1-成果 增加体检结果详情

--- healthy-frontend/src/components/details/Details.vue
+++ healthy-frontend/src/components/details/Details.vue
@@ -1,7 +1,91 @@
 <template>
-  <div></div>
+  <div>
+    <el-tabs type="border-card">
+      <el-tab-pane label="User">
+        <el-table :data="tableData" height="250" border style="width: 100%">
+          <el-table-column prop="id" label="#" width="60" />
+          <el-table-column prop="name" label="体检项目" />
+          <el-table-column prop="result" label="明细结果">
+            <template #default="{row}">
+              <el-input v-model="row.result" placeholder="-" />
+            </template>
+          </el-table-column>
+          <el-table-column prop="unit" label="单位" />
+          <el-table-column prop="reference" label="参考值" />
+          <el-table-column prop="prompt" label="提示" />
+          <el-table-column prop="abnormal" label="异常">
+            <template #default="{row}">
+              <el-checkbox v-model="row.abnormal" label="是" size="large" />
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="Config">Config</el-tab-pane>
+      <el-tab-pane label="Role">Role</el-tab-pane>
+      <el-tab-pane label="Task">Task</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const tableData = [
+  {
+    id: '1',
+    name: 'Tom',
+    result: '正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: false,
+  },
+  {
+    id: '1',
+    name: 'Tom',
+    result: '不正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: true,
+  },
+  {
+    id: '1',
+    name: 'Tom',
+    result: '正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: false,
+  },
+  {
+    id: '1',
+    name: 'Tom',
+    result: '正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: false,
+  },
+  {
+    id: '1',
+    name: 'Tom',
+    result: '正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: false,
+  },
+  {
+    id: '1',
+    name: 'Tom',
+    result: '不正常',
+    unit: '-',
+    reference: '114514',
+    prompt: '-',
+    abnormal: true,
+  },
+]
+</script>
 
 <style lang="scss" scoped></style>
