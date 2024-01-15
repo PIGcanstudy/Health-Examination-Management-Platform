5af4a30c2ed2bc519e717b888c5bfbc3034dc324
zjf121 <3352629190@qq.com>
2024-01-15 20:43:39 +0800

    定义接口包、接口

---
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j3/message/MessageApis.java
@@ -0,0 +1,8 @@
+package com.zeroone.star.project.j3.message;
+/**
+ * @description:消息管理Api
+ * @author: 坚强少年
+ * @date: 2024/1/15 20:36
+ **/
+public interface MessageApis {
+}
---
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j3/typelib/TypeLibApis.java
@@ -0,0 +1,8 @@
+package com.zeroone.star.project.j3.typelib;
+/**
+ * @description:类型库api
+ * @author: 坚强少年
+ * @date: 2024/1/15 20:36
+ **/
+public interface TypeLibApis {
+}
