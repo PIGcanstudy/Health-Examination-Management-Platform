3e33778efa7d2dc432ab1bda5663e75e698ea391
puck <1093308061@qq.com>
2024-01-15 17:27:47 +0800

    新增查询结果规则列表接口，还有bug， --puck

---
+++ healthy-cpp/health-c2-bas/controller/positive/resRuleController.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "resRuleController.h"
+#include "../ApiDeclarativeServicesHelper.h"
+
+resRuleListJsonVO::Wrapper resRuleController::execQueryResRule(const resRuleListQuery::Wrapper& query)
+{
+	return {};
+}
