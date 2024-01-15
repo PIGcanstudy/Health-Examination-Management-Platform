0260bf9b8dcabbadd1b165e778143a0589c85de5
luoyiyi <354651720@qq.com>
2024-01-15 18:52:26 +0800

    commit - f1-lyy

--- healthy-frontend/src/components/navigation/Navigation.vue
+++ healthy-frontend/src/components/navigation/Navigation.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+
+
 </template>
 
 <script setup></script>
