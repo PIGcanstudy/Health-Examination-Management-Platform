a472b7eff67883027c483a85adb5acda4ff5cb1f
Holy_Grail <3281812061@qq.com>
2024-01-15 22:50:15 +0800

    新增取消弃检领域模型接口

--- healthy-cpp/healthy-c6-resultentry/controller/Router.cpp
+++ healthy-cpp/healthy-c6-resultentry/controller/Router.cpp
@@ -27,6 +27,8 @@
 #include "uselib/ws/WSController.h"
 #endif
 
+#include "result-entry/ClAbanProjController.h"
+
 // 如果定义了关闭Swagger文档宏
 #ifdef CLOSE_SWAGGER_DOC
 // 简化绑定控制器宏定义
@@ -51,6 +53,8 @@ void Router::initRouter()
 #endif
 
 	//#TIP :系统扩展路由定义，写在这个后面
+	ROUTER_SIMPLE_BIND(ClAbanProjController);
+
 
 }
 
