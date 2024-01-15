8338debbf3f41607d039070d98a8cd8d55a45bc5
camille <camille3401424303@163.com>
2024-01-15 17:40:35 +0800

    新增Message接口

--- healthy-java/healthy-j2-message/src/main/java/com/zeroone/star/message/controller/TMessageSendController.java
+++ healthy-java/healthy-j2-message/src/main/java/com/zeroone/star/message/controller/TMessageSendController.java
@@ -1,6 +1,14 @@
 package com.zeroone.star.message.controller;
 
 
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.j2.MessageApis;
+import com.zeroone.star.project.j2.dto.MessageDTO;
+import com.zeroone.star.project.j2.query.MessageQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-01-15
  */
 @RestController
+@Api(tags = "消息详情")
 @RequestMapping("/message/t-message-send")
-public class TMessageSendController {
-
+public class TMessageSendController implements MessageApis {
+    @ApiOperation("获取消息详情")
+    @GetMapping("MessageSend")
+    @Override
+    public JsonVO<PageDTO<MessageDTO>> queryMessage(MessageQuery messageQuery) {
+        return null;
+    }
 }
 
