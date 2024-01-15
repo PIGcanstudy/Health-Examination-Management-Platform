09bc759c8cb138a54d549c69105070de39abf5a3
light <2587891565@qq.com>
2024-01-15 20:18:41 +0800

    完成修改密码与重置密码对应的接口文档的书写

--- healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/j1/dto/sysmanager/ModifyPasswordDTO.java
+++ healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/j1/dto/sysmanager/ModifyPasswordDTO.java
@@ -1,10 +1,11 @@
-package com.zeroone.star.project.j1.dto;
+package com.zeroone.star.project.j1.dto.sysmanager;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -12,17 +13,19 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel("修改密码数据对象")
-public class ChangePasswordDTO {
-    @NotBlank(message = "密码不能为空")
+public class ModifyPasswordDTO {
+
+    @NotBlank(message = "旧密码不能为空")
     @ApiModelProperty(value = "旧密码", example = "123456",required = true)
     private String password;
 
+    @Length(message = "密码长度不能超过{max}个",max = 20)
     @NotBlank(message = "密码不能为空")
     @ApiModelProperty(value = "新密码", example = "12345678",required = true)
     private String new_password;
 
-    @NotBlank(message = "用户id不能为空")
-    @ApiModelProperty(value = "用户id", example = "682265633886208",required = true)
+    @NotBlank(message = "被修改密码的用户的id不能为空")
+    @ApiModelProperty(value = "被修改密码的用户的id", example = "682265633886208",required = true)
     private String id;
 
 
