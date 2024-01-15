7a6ec8993c084902814cbf40f1049f4f7c738672
ChengcanGuo <2488700263@qq.com>
2024-01-15 21:12:13 +0800

    添加了图标和部分功能

--- healthy-frontend/src/components/search/Search.vue
+++ healthy-frontend/src/components/search/Search.vue
@@ -1,31 +1,33 @@
 <template>
   <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-form-item label="单位名称">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
+    <el-form-item label="单位名称" class="bold-label">
+      <el-input v-model="formInline.name" placeholder="请输入" clearable />
     </el-form-item>
-    <el-form-item label="信用代码">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
+    <el-form-item label="信用代码" class="bold-label">
+      <el-input v-model="formInline.code" placeholder="请输入" clearable />
     </el-form-item>
-    <el-form-item label="联系人">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
+    <el-form-item label="联系人" class="bold-label">
+      <el-input v-model="formInline.contact" placeholder="请输入" clearable />
     </el-form-item>
-    <el-form-item label="联系电话">
-      <el-input v-model="formInline.user" placeholder="请输入" clearable />
+    <el-form-item label="联系电话" class="bold-label">
+      <el-input v-model="formInline.phone" placeholder="请输入" clearable />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSearch">搜索</el-button>
+      <el-button type="primary" :icon="Search" @click="onSearch">搜索</el-button>
       <el-button plain @click="onReset">重置</el-button>
     </el-form-item>
   </el-form>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 import { reactive } from 'vue'
+import { Search } from '@element-plus/icons-vue'
 
 const formInline = reactive({
-  user: '',
-  region: '',
-  date: ''
+  name: '',
+  code: '',
+  contact: '',
+  phone: ''
 })
 
 /**
@@ -41,6 +43,10 @@ const onSearch = () => {
  */
 const onReset = () => {
   console.log('reset!')
+  // 将表单中的输入框清空
+  for (const key in formInline) {
+    formInline[key] = ''
+  }
 }
 </script>
 
@@ -49,7 +55,7 @@ const onReset = () => {
   --el-input-width: 220px;
 }
 
-.demo-form-inline .el-select {
-  --el-select-width: 220px;
+.bold-label .el-form-item__label {
+  font-weight: bold;
 }
 </style>
