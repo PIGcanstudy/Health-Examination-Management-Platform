0cda44561351c0f88f8a40071532f0ca4952dfd3
trx open <2475679215@qq.com>
2024-01-16 20:37:15 +0800

    创建会跑的鱼所属工作分支

--- healthy-cpp/healthy-c5-QueStatistics/main.cpp
+++ healthy-cpp/healthy-c5-QueStatistics/main.cpp
@@ -51,6 +51,7 @@ bool getStartArg(int argc, char* argv[]) {
 	std::string dbPassword = "123456";
 	std::string dbName = "test";
 	std::string dbHost = "192.168.220.128";
+	//std::string dbHost = "192.168.220.128";
 	int dbPort = 3306;
 	int dbMax = 5;
 #ifdef LINUX
