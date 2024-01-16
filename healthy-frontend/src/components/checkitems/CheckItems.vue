b0c436c2af587f7a28dd940923adaab8894652ec
275258700@qq.com <275258700@qq.com>
2024-01-16 21:32:12 +0800

    60%进度抽屉

--- healthy-frontend/src/components/checkitems/CheckItems.vue
+++ healthy-frontend/src/components/checkitems/CheckItems.vue
@@ -1,23 +1,65 @@
 <template>
-  <el-button type="primary" style="margin-left: 16px" @click="drawer = true">套餐项目</el-button>
+  <el-button type="primary" style="margin-left: 16px" @click="drawer = true">套餐类型</el-button>
+  <el-button type="primary" style="margin-left: 16px" @click="drawer2 = true">选检项目</el-button>
 
   <el-drawer v-model="drawer" title="I am the title" :with-header="false">
-    <span>套餐项目</span>
+    <span>从业套餐选择</span>
     <div class="demo-input-suffix">
     <el-row :gutter="20">
-      <span class="ml-3 w-35 text-gray-600 inline-flex items-center"
+      <span class="suibian"
         >关键字: </span
       >
       <el-input
         v-model="input1"
-        class="w-50 m-2"
+        class="suibian"
+        width="50px"
         placeholder="请输入关键字"
         :suffix-icon="Search"
       />
-      <el-button type="primary" @click="handleSearch">查询</el-button>
-      <el-button @click="handleCz">重置</el-button>
+      <el-button type="primary"  class="suibian" @click="handleSearch">查询</el-button>
+      <el-button @click="handleCz" class="suibian">重置</el-button>
     </el-row>
   </div>
+
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="xz" label="XZ" width="180" />
+    <el-table-column prop="tcmc" label="TCMC" width="180" />
+    <el-table-column prop="tcjp" label="TCJP" />
+  </el-table>
+  </el-drawer>
+
+  <el-drawer v-model="drawer2" title="I am the title" :with-header="false">
+    <span>套餐项目</span>
+    <div class="demo-input-suffix">
+    <el-row :gutter="20">
+      <span class="suibian"
+        >关键字: </span
+      >
+      <el-input
+        v-model="input1"
+        class="suibian"
+        width="50px"
+        placeholder="请输入关键字"
+        :suffix-icon="Search"
+      />
+      <el-button type="primary"  class="suibian" @click="handleSearch">查询</el-button>
+      <el-button @click="handleCz" class="suibian">重置</el-button>
+    </el-row>
+  </div>
+
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="xz" label="XZ" width="180" />
+    <el-table-column prop="tcmc" label="TCMC" width="180" />
+    <el-table-column prop="tcjp" label="TCJP" />
+  </el-table>
   </el-drawer>
 </template>
 
@@ -27,6 +69,7 @@ import { Calendar, Search } from '@element-plus/icons-vue'
 // import { ArrowDown } from '@element-plus/icons-vue'
 
 const drawer = ref(false)
+const drawer2 = ref(false)
 const input1 = ref('')
 // const handleCommand = (command: string | number | object) => {
 //   console.log(document.getElementById('keshi'))
@@ -41,6 +84,50 @@ const handleSearch = () => {
 const handleCz = () => {
   console.log('Cz')
 }
+
+interface User {
+  xz: string
+  tcmc: string
+  tcjp: string
+}
+
+const tableRowClassName = ({
+  row,
+  rowIndex,
+}: {
+  row: User
+  rowIndex: number
+}) => {
+  if (rowIndex === 1) {
+    return 'warning-row'
+  } else if (rowIndex === 3) {
+    return 'success-row'
+  }
+  return ''
+}
+
+const tableData: User[] = [
+  {
+    xz: '2016-05-03',
+    tcmc: '优惠套餐-ms',
+    tcjp: 'YHTC',
+  },
+  {
+    xz: '2016-05-02',
+    tcmc: '健康证',
+    tcjp: 'CYTJ',
+  },
+  {
+    xz: '2016-05-04',
+    tcmc: 'Tom',
+    tcjp: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    xz: '2016-05-01',
+    tcmc: 'Tom',
+    tcjp: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
 
 <style>
@@ -49,5 +136,10 @@ const handleCz = () => {
 }
 .el-table .success-row {
   --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+.suibian{
+  display: flex;
+      justify-content: space-between;
+      align-items: center;
 }
 </style>
