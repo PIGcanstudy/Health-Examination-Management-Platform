be835141757494177730814e92f7de50e8027105
yjq <2461771510@qq.com>
2024-01-16 16:59:33 +0800

    增加消息和类型库的领域模型和api接口

--- healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j3/message/MessageApis.java
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j3/message/MessageApis.java
@@ -1,8 +1,27 @@
 package com.zeroone.star.project.j3.message;
+
+import com.zeroone.star.project.dto.j3.message.EditMessageDTO;
+import com.zeroone.star.project.dto.j3.message.SendMessageDTO;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * @description:消息管理Api
  * @author: 坚强少年
  * @date: 2024/1/15 20:36
  **/
 public interface MessageApis {
+
+    /**
+     * 发送消息
+     * @param sendMessageDTO
+     * @return
+     */
+    JsonVO<String> sendMessage(SendMessageDTO sendMessageDTO);
+
+    /**
+     * 修改消息
+     * @param editMessageDTO
+     * @return
+     */
+    JsonVO<String> editMessage(EditMessageDTO editMessageDTO);
 }
--- healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/MessageController.java
+++ healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/MessageController.java
@@ -1,11 +1,17 @@
 package com.zeroone.star.sysmanager.controller;
 
-import com.zeroone.star.project.j3.dto.MessageDTO;
+import com.zeroone.star.project.dto.j3.message.EditMessageDTO;
+import com.zeroone.star.project.dto.j3.message.SendMessageDTO;
 import com.zeroone.star.project.j3.message.MessageApis;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * 消息管理Controller
@@ -18,4 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("消息控制")
 public class MessageController implements MessageApis {
 
+    @ApiOperation("发送消息")
+    @PostMapping("send-Message")
+    @Override
+    public JsonVO<String> sendMessage(@Valid @RequestBody SendMessageDTO sendMessageDTO) {
+        return null;
+    }
+
+    @ApiOperation("修改消息")
+    @PostMapping("edit-Message")
+    @Override
+    public JsonVO<String> editMessage(@Valid @RequestBody EditMessageDTO editMessageDTO) {
+        return null;
+    }
 }
