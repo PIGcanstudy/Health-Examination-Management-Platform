7294ce8f714e448491b0e077543c9e790691abe7
zjf121 <3352629190@qq.com>
2024-01-16 22:50:14 +0800

    重新定义MVC

--- healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/mapper/MessageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zeroone.star.sysmanager.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zeroone.star.project.dto.j3.message.MessageDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MessageMapper extends BaseMapper<MessageDTO> {
-}
