7ba389d07431ffb1024966fab6dd47c60eeae979
hualiuliu <hualiuliu6767@163.com>
2024-01-17 21:57:17 +0800

    菜单功能开发-01（查询菜单，添加菜单）

---
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j2/PermissionApis.java
@@ -0,0 +1,24 @@
+package com.zeroone.star.project.j2;
+
+import com.zeroone.star.project.j2.dto.PermissionDTO;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
+/**
+ * @author 滑溜溜
+ * @version 1.0.0
+ * @desc 菜单操作api
+ * @since 2024-01-17
+ */
+public interface PermissionApis {
+//JsonVO<PageDTO<MessageDTO>> queryMessage(MessageQuery messageQuery);
+
+    public JsonVO<List<PermissionDTO>> getAllMenuList(); //获取用户页面菜单数据
+
+    public JsonVO<List<PermissionDTO>> getAllList();  //获取权限菜单树
+
+    public JsonVO<PermissionDTO> add(PermissionDTO permission);
+
+
+}
