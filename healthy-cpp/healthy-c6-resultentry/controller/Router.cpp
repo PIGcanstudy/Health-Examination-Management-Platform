5c569726314553999a8972cf47b83a495b1d20d1
haibara ai <2296396559@qq.com>
2024-01-19 02:16:52 +0800

    修改原有代码

--- healthy-cpp/healthy-c6-resultentry/controller/Router.cpp
+++ healthy-cpp/healthy-c6-resultentry/controller/Router.cpp
@@ -19,6 +19,7 @@
 #include "stdafx.h"
 #include "Router.h"
 #include "ApiHelper.h"
+#include "unknownmodule/UnknownModuleController.h"
 
 #ifdef HTTP_SERVER_DEMO
 #include "user/UserController.h"
@@ -51,7 +52,11 @@ void Router::initRouter()
 #endif
 
 	//#TIP :系统扩展路由定义，写在这个后面
-
+	ROUTER_SIMPLE_BIND(UnknownModuleController);
+	// ROUTER_SIMPLE_BIND(BasicInformationController);
+	// ROUTER_SIMPLE_BIND(LifeHistoryContronller);
+	// ROUTER_SIMPLE_BIND(PastMedicalHistoryContronller);
+	// ROUTER_SIMPLE_BIND(ProfessionalHistoryContronller);
 }
 
 #ifdef HTTP_SERVER_DEMO
