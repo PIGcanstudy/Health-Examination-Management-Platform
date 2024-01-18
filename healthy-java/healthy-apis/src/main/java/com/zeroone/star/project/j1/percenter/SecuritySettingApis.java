2a8ddd89ef74492fcc544e27f1560598f24ed712
Yun Feng <1271383559@qq.com>
2024-01-18 22:00:35 +0800

    修改 接口与领域模型定义-修改密码、修改手机模块 修改controller返回值类型、删除VO

--- healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j1/percenter/SecuritySettingApis.java
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j1/percenter/SecuritySettingApis.java
@@ -2,8 +2,6 @@ package com.zeroone.star.project.j1.percenter;
 
 import com.zeroone.star.project.j1.dto.percenter.ModifyPasswordDTO;
 import com.zeroone.star.project.j1.dto.percenter.ModifyPhoneDTO;
-import com.zeroone.star.project.j1.vo.percenter.ModifyPasswordVO;
-import com.zeroone.star.project.j1.vo.percenter.ModifyPhoneVO;
 import com.zeroone.star.project.vo.JsonVO;
 
 /**
@@ -11,18 +9,18 @@ import com.zeroone.star.project.vo.JsonVO;
  */
 public interface SecuritySettingApis {
     /**
-     *  修改密码接口
+     * 修改密码接口
      *
      * @param modifyPasswordVO 修改密码视图对象
      * @return 修改结果
      */
-     JsonVO<ModifyPasswordDTO> changePassword(ModifyPasswordVO modifyPasswordVO);
+     JsonVO<String> changePassword(ModifyPasswordDTO modifyPasswordVO);
 
     /**
-     *  修改手机接口
+     * 修改手机接口
      *
      * @param modifyPhoneVO 修改手机号视图对象
      * @return 修改结果
      */
-    JsonVO<ModifyPhoneDTO> changePhone(ModifyPhoneVO modifyPhoneVO);
+    JsonVO<String> changePhone(ModifyPhoneDTO modifyPhoneVO);
 }
--- healthy-java/healthy-j1-percenter/src/main/java/com/zeroone/star/percenter/controller/SecuritySettingController.java
+++ healthy-java/healthy-j1-percenter/src/main/java/com/zeroone/star/percenter/controller/SecuritySettingController.java
@@ -3,8 +3,6 @@ package com.zeroone.star.percenter.controller;
 import com.zeroone.star.project.j1.dto.percenter.ModifyPasswordDTO;
 import com.zeroone.star.project.j1.dto.percenter.ModifyPhoneDTO;
 import com.zeroone.star.project.j1.percenter.SecuritySettingApis;
-import com.zeroone.star.project.j1.vo.percenter.ModifyPasswordVO;
-import com.zeroone.star.project.j1.vo.percenter.ModifyPhoneVO;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,15 +18,15 @@ public class SecuritySettingController implements SecuritySettingApis {
     @ApiOperation(value = "修改密码", notes = "用户修改自己的密码")
     @PostMapping("/changePassword")
     @Override
-    public JsonVO<ModifyPasswordDTO> changePassword(@RequestBody ModifyPasswordVO modifyPasswordVO) {
+    public JsonVO<String> changePassword(@RequestBody ModifyPasswordDTO modifyPasswordDTO) {
         System.out.println("修改密码");
-        return JsonVO.success(null);
+        return JsonVO.success("修改成功");
     }
     @ApiOperation(value = "修改手机号", notes = "用户修改自己的手机号码")
     @PostMapping("/changePhone")
     @Override
-    public JsonVO<ModifyPhoneDTO> changePhone(@RequestBody ModifyPhoneVO modifyPhoneVO) {
+    public JsonVO<String> changePhone(@RequestBody ModifyPhoneDTO modifyPhoneDTO) {
         System.out.println("修改手机号");
-        return JsonVO.success(null);
+        return JsonVO.success("修改成功");
     }
 }
