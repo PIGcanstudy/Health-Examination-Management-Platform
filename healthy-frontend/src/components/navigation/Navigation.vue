c070a10632af529d694db9997f77145a43a83c29
luoyiyi <354651720@qq.com>
2024-01-18 21:40:01 +0800

    洛翼忆导航栏组件完成1-18

--- healthy-frontend/src/components/navigation/Navigation.vue
+++ healthy-frontend/src/components/navigation/Navigation.vue
@@ -9,11 +9,10 @@
             </router-link>
             <el-text  size="large" >团检单位</el-text>
           </div>
-
-          <!-- <div class="arrow01">
-            <img src="../../assets/navigation/arrow01.png"/>
-          </div>  -->
-          <!-- <img src="../../assets/navigation/arrow01.png"/> -->
+          <div class="arrow01">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+          
 
           <div class="ic02">
             <router-link to="/sample/excel">
@@ -21,66 +20,122 @@
           </router-link>
             <el-text  size="large">团检订单</el-text>
           </div>
+          <div class="arrow02">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic03">
             <router-link to="/sample/print">
             <img src="../../assets/navigation/icon03.png" />
             </router-link>
             <el-text  size="large">团检人员</el-text>
           </div>
+          <div class="arrow03">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
+
+
           <div class="ic04">
             <router-link to="/sample/print">
             <img src="../../assets/navigation/icon04.png" />
             </router-link>
             <el-text  size="large">合同详情</el-text>
           </div>
+          <div class="arrow04">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
+
           <div class="ic05">
             <router-link to="/sample/print">
             <img src="../../assets/navigation/icon05.png" />
             </router-link>
             <el-text  size="large">单位报告</el-text>
           </div>
+          <div class="arrow05">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic06">
             <router-link to="/sample/print">
             <img src="../../assets/navigation/icon06.png" />
             </router-link>
             <el-text  size="large">体检诊台</el-text>
           </div>
+          <div class="arrow06">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic07">
             <router-link to="/sample/pdfobject">
             <img src="../../assets/navigation/icon07.png" />
             </router-link>
             <el-text  size="large">医生总检</el-text>
           </div>
+          <div class="arrow07">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic08">
             <router-link to="/sample/basedatalist">
             <img src="../../assets/navigation/icon08.png" />
             </router-link>
             <el-text  size="large">报告审核</el-text>
           </div>
+          <div class="arrow08">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic09">
             <router-link to="/sample/basedatalisttest">
             <img src="../../assets/navigation/icon09.png" />
             </router-link>
             <el-text  size="large">体检报告</el-text>
           </div>
+          <div class="arrow09">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+
           <div class="ic10">
             <router-link to="/sample/checkitems">
             <img src="../../assets/navigation/icon10.png" />
             </router-link>
             <el-text  size="large">查询统计</el-text>
           </div>
+          <div class="arrow10">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
+          
+
           <div class="ic11">
             <router-link to="/sample/search">
             <img src="../../assets/navigation/icon11.png" />
             </router-link>
             <el-text  size="large">体检登记</el-text>
           </div>
+          <div class="arrow11">
+            <img src="../../assets/navigation/arrow03.png"/>
+          </div> 
           <div class="ic12">
             <router-link to="/sample/medicalinfo">
             <img src="../../assets/navigation/icon12.png" />
             </router-link>
             <el-text  size="large">数据网报</el-text>
-          </div>       
+          </div>      
+          
+          <div class="line">
+            <img src="../../assets/navigation/row03.png"/>
+          </div> 
+          <div class="line01">
+            <img src="../../assets/navigation/row01.png"/>
+          </div> 
+          <div class="row01">
+            <img src="../../assets/navigation/row02.png"/>
+          </div> 
+          <div class="row02">
+            <img src="../../assets/navigation/row02.png"/>
+          </div> 
         </el-main>
         
       </el-container>
@@ -223,8 +278,141 @@
   text-align: center;
 }
 .arrow01{
-  top: 60px;
-  left :140px ;
+  top: 70px;
+  left :172px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.arrow02{
+  top: 5px;
+  left :295px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+
+.arrow03{
+  top: -60px;
+  left :415px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.arrow04{
+  top: -110px;
+  left :1070px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+//10
+.arrow05{
+  top: 70px;
+  left :1070px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+//12
+.arrow06{
+  top: 270px;
+  left :1070px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+//9
+.arrow07{
+  top: -55px;
+  left :915px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.arrow08{
+  top: -120px;
+  left :790px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+
+.arrow09{
+  top: -185px;
+  left :670px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.arrow10{
+  top: -250px;
+  left :560px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.arrow11{
+  top: -320px;
+  left :1030px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+//右边直立的直线
+.line{
+  top: -630px;
+  left :-275px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+//左边直立的直线
+.line01{
+  top: -615px;
+  left :550px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.row01{
+  top: -630px;
+  left :517px ;
+  height:1px;
+  width: 40px;
+  margin: 0 ;
+  border: 0 ;
+  padding: 0;
+}
+.row02{
+  top: -127px;
+  left :517px ;
   height:1px;
   width: 40px;
   margin: 0 ;
