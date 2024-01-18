d6a9e3ff4bae08e4d6211cbeca410737fd5d4af7
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-01-18 23:12:18 +0800

    新增领域模型数据定义VO DTO Query
    新增领域模型数据定义VO DTO Query

---
+++ healthy-cpp/healthy-c7-common/domain/query/evaluate/InquiryDetailQuery.h
@@ -0,0 +1,45 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: lenya~
+ @Date: 2024/1/18
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _DETAILQUERY_H_
+#define _DETAILQUERY_H_
+
+#include "../../GlobalInclude.h"
+#include "domain/query/PageQuery.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * 获取问诊明细分页查询
+ */
+class InquiryDetailQuery : public PageQuery
+{
+	// 定义初始化
+	DTO_INIT(InquiryDetailQuery, PageQuery);
+	// 姓名
+	API_DTO_FIELD_DEFAULT(String, name, ZH_WORDS_GETTER("inquiry.field.name"));
+	// 体检编号 
+	API_DTO_FIELD_DEFAULT(String, checkId, ZH_WORDS_GETTER("inquiry.field.checkId"));
+	// 单位名称
+	API_DTO_FIELD_DEFAULT(String, unitName, ZH_WORDS_GETTER("inquiry.field.unitName"));
+
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // !_USERQUERY_H_
