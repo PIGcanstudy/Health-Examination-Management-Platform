e5595454455d500d8155218cb347ca6ffcd544df
杨欣 <2086958541@qq.com>
2024-01-18 21:03:02 +0800

    再次提交

---
+++ healthy-cpp/healthy-c5-ExaLanding/controller/Exalanding/PeopleListController.cpp
@@ -0,0 +1,7 @@
+#include "PeopleListController.h"
+#include "stdafx.h"
+
+PeopleListPageJsonVO::Wrapper PeopleListController::execQueryPeopleList(const PeopleListQuery::Wrapper& query)
+{
+	return {};
+}
