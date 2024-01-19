cf72080b37405431a465c21996e37cd346e8fc9f
goriot <1528917116@qq.com>
2024-01-19 22:42:42 +0800

    完成domain

---
+++ healthy-cpp/health-c2-market/controller/groupapprove/GroupApproveController.cpp
@@ -0,0 +1,81 @@
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: awei
+ @Date: 2022/10/25 10:58:42
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include "stdafx.h"
+#include "GroupApproveController.h"
+#include "../../service/sample/SampleService.h"
+#include "../ApiDeclarativeServicesHelper.h"
+
+
+Uint64JsonVO::Wrapper GroupApproveController::execAddGroupApprove(const GroupApproveDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 参数校验
+	// 非空校验
+	if (!dto->age || !dto->name || !dto->sex)
+	{
+		jvo->init(UInt64(-1), RS_PARAMS_INVALID);
+		return jvo;
+	}
+	// 有效值校验
+	if (dto->age < 0 || dto->name->empty() || dto->sex->empty())
+	{
+		jvo->init(UInt64(-1), RS_PARAMS_INVALID);
+		return jvo;
+	}
+
+	// 定义一个Service
+	GroupApproveService service;
+	// 执行数据新增
+	uint64_t id = service.saveData(dto);
+	if (id > 0) {
+		jvo->success(UInt64(id));
+	}
+	else
+	{
+		jvo->fail(UInt64(id));
+	}
+	//响应结果
+	return jvo;
+}
+
+Uint64JsonVO::Wrapper GroupApproveController::execModifyGroupApprove(const GroupApproveDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 参数校验
+	if (!dto->id || dto->id <= 0)
+	{
+		jvo->init(UInt64(-1), RS_PARAMS_INVALID);
+		return jvo;
+	}
+	// 定义一个Service
+	GroupApproveService service;
+	// 执行数据修改
+	if (service.updateData(dto)) {
+		jvo->success(dto->id);
+	}
+	else
+	{
+		jvo->fail(dto->id);
+	}
+	// 响应结果
+	return jvo;
+}
+
