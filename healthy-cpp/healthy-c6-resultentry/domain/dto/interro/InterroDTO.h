a76e1c9933a52c1fcd1750df49fe03a24abb1e08
zehua0417 <zehuali0417@gmail.com>
2024-01-22 21:36:55 +0800

    domain finished

---
+++ healthy-cpp/healthy-c6-resultentry/domain/dto/interro/InterroDTO.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef _INTERRO_DTO_
+#define _INTERRO_DTO_
+#include "../../GlobalInclude.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+/*
+* 症状询问传输对象
+*/
+class InterroDTO : public oatpp::DTO {
+	DTO_INIT(InterroDTO, DTO);
+	DTO_FIELD(String, id);
+	DTO_FIELD_INFO(id) {
+		info->description = ZH_WORDS_GETTER("interro1.field.id");
+	}
+	// 体检项目
+	DTO_FIELD(String, project);
+	DTO_FIELD_INFO(project) {
+		info->description = ZH_WORDS_GETTER("interro1.field.project1");
+	}
+	// 危急程度
+	DTO_FIELD(String, degree);
+	DTO_FIELD_INFO(degree) {
+		info->description = ZH_WORDS_GETTER("interro1.field.degree");
+	}
+	// 病程时间
+	DTO_FIELD(String, time);
+	DTO_FIELD_INFO(time) {
+		info->description = ZH_WORDS_GETTER("interro1.field.time");
+	}
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // _INTERRO_DTO_
