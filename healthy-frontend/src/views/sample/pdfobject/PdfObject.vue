ee85361655e648d4584dd150120519480fc0d899
275258700@qq.com <275258700@qq.com>
2024-01-23 17:23:21 +0800

    Revert "Merge commit '3b886c2788f6163b32e3d50b2e97bad793d4cbba' into f1"
    This reverts commit afa91ef2744cff59d553e49712598929dec9fc8e, reversing
    changes made to c0bd349840e1a5c3d669215fd4cab11b5c55b9f4.

--- healthy-frontend/src/views/sample/pdfobject/PdfObject.vue
+++ healthy-frontend/src/views/sample/pdfobject/PdfObject.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <PdfObject></PdfObject>
-  </div>
+  <div id="my-container"></div>
 </template>
 <script setup>
-import PdfObject from '@/components/pdfobject/PdfObject.vue'
+//导入PDFObject
+import PDFObject from 'pdfobject'
+import { onMounted } from 'vue'
+onMounted(() => {
+  let url = 'https://pdfobject.com/pdf/sample-3pp.pdf'
+  PDFObject.embed(url, '#my-container')
+})
 </script>
-<style scoped></style>
+<style scoped>
+/* 设置pdf样式，类名固定 */
+.pdfobject-container {
+  height: 680px;
+  border: 1rem solid rgba(0, 0, 0, 0.1);
+}
+</style>
