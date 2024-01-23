7cce6118dcf7c9d8726de3ff06b36e7ed736dfd2
whx <2623674073@qq.com>
2024-01-23 17:19:34 +0800

    恢复版本

---
+++ healthy-cpp/healthy-c7-common/domain/query/evalue/InquiryDetailQuery.h
@@ -0,0 +1,63 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: lenya~
+ @Date: 2024/1/18
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef  _DETAILQUERY_
+#define  _DETAILQUERY_
+
+#include "../../GlobalInclude.h"
+#include "domain/query/PageQuery.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * 获取问诊明细分页查询
+ */
+class InquiryDetailQuery : public PageQuery
+{
+	// 定义初始化
+	DTO_INIT(InquiryDetailQuery, PageQuery);
+
+	// 工龄年段
+	API_DTO_FIELD_DEFAULT(Int32, workYear, ZH_WORDS_GETTER("inquiry.field.workYear"));
+	// 工龄月段
+	API_DTO_FIELD_DEFAULT(Int32, workMonth, ZH_WORDS_GETTER("inquiry.field.workMonth"));
+	// 结婚状况
+	API_DTO_FIELD_DEFAULT(String, maritalStatus, ZH_WORDS_GETTER("inquiry.field.maritalStatus"));
+	//按需年龄
+	API_DTO_FIELD_DEFAULT(Int32, needYear, ZH_WORDS_GETTER("inquiry.field.needYear"));
+	//按需月龄
+	API_DTO_FIELD_DEFAULT(Int32, needmonth, ZH_WORDS_GETTER("inquiry.field.needMonth"));
+
+	//非必须
+
+	//文化程度
+	API_DTO_FIELD_DEFAULT(String, culture, ZH_WORDS_GETTER("inquiry.field.culture"));
+	//家庭地址
+	API_DTO_FIELD_DEFAULT(String, homeAddress, ZH_WORDS_GETTER("inquiry.field.homeAddress"));
+	//工种名称
+	API_DTO_FIELD_DEFAULT(String, jobName, ZH_WORDS_GETTER("inquiry.field.jobName"));
+	//工种其他名称
+	API_DTO_FIELD_DEFAULT(String, jobOrderName, ZH_WORDS_GETTER("inquiry.field.jobOrderName"));
+	//所属部门
+	API_DTO_FIELD_DEFAULT(String, department, ZH_WORDS_GETTER("inquiry.field.department"));
+
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // !_USERQUERY_H_
