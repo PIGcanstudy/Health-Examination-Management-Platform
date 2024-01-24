f4960916047f2f5f90f539747d925bf4e9d15bb0
trx open <2475679215@qq.com>
2024-01-24 10:18:58 +0800

    创建获取体检结论列表和获取体检单位订单名称列表领域模型和接口定义

---
+++ healthy-cpp/healthy-c5-QueStatistics/controller/ordername/OrderNameController.cpp
@@ -0,0 +1,8 @@
+
+#include "stdafx.h"
+#include "OrderNameController.h"
+
+OrderNamePageJsonVO::Wrapper OrderNameController::execQueryOrderName(const OrderNameQuery::Wrapper& query, const PayloadDTO& payload)
+{
+	return OrderNamePageJsonVO::Wrapper();
+}
---
+++ healthy-cpp/healthy-c5-QueStatistics/controller/resultslist/ResultsListController.cpp
@@ -0,0 +1,8 @@
+
+#include "stdafx.h"
+#include "ResultsListController.h"
+
+ResultsListPageJsonVO::Wrapper ResultsListController::execQueryResultsList(const ResultsListQuery::Wrapper& query, const PayloadDTO& payload)
+{
+	return ResultsListPageJsonVO::Wrapper();
+}
