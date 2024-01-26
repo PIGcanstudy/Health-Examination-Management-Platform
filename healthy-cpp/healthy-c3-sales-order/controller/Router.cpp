25b17cc6f55ee8562d0b43e2c767562b1245a92d
kzz <kzz@qq.com>
2024-01-27 07:30:52 +0800

    合并order部分

--- healthy-cpp/healthy-c3-sales-order/controller/Router.cpp
+++ healthy-cpp/healthy-c3-sales-order/controller/Router.cpp
@@ -20,12 +20,24 @@
 #include "Router.h"
 #include "ApiHelper.h"
 
+#include "delete-order/RemoveOrderController.h"
+#include "get-order-item/QueryItemController.h"
+
 #ifdef HTTP_SERVER_DEMO
 #include "user/UserController.h"
 #include "sample/SampleController.h"
 #include "file/FileController.h"
 #include "uselib/ws/WSController.h"
 #endif
+#include "get-order-list/QueryOrderListController.h"
+#include "get-order-detail/QueryOrderDetailController.h"
+#include "download-entrust-template/DownloadEntrustController.h"
+#include "get-order-person/QueryPersonController.h"
+#include "add-order/AddOrderController.h"
+#include "get-order-approval/QueryOrderApprovalController.h"
+#include "export-person/ExportPersonController.h"
+
+
 
 // 如果定义了关闭Swagger文档宏
 #ifdef CLOSE_SWAGGER_DOC
@@ -51,7 +63,47 @@ void Router::initRouter()
 #endif
 
 	//#TIP :系统扩展路由定义，写在这个后面
+	/*------  订单  ------*/
+	/*------  获取订单名称列表（条件+分页）    负责人：Chloe  ------*/
+	ROUTER_SIMPLE_BIND(QueryOrderListController);
 
+	/*------  获取订单详情    负责人：星星果  ------*/
+	ROUTER_SIMPLE_BIND(QueryOrderDetailController);
+
+	/*------  获取订单体检项目信息    负责人：Adam  ------*/
+	ROUTER_SIMPLE_BIND(QueryItemController);
+
+	/*------  获取订单体检人员信息    负责人：ronaldo  ------*/
+	ROUTER_SIMPLE_BIND(QueryPersonController);
+
+	/*------  新增订单    负责人：ronaldo  ------*/
+	ROUTER_SIMPLE_BIND(AddOrderController);
+
+	/*------  修改订单    负责人：jimyy  ------*/
+
+	/*------  删除订单    负责人：Adam  ------*/
+	ROUTER_SIMPLE_BIND(RemoveOrderController);
+
+	/*------  订单确认    负责人：jimyy  ------*/
+
+	/*------  查看审批信息    负责人：无敌霸王龙  ------*/
+	ROUTER_SIMPLE_BIND(QueryOrderApprovalController);
+
+	/*------  人员  ------*/
+	/*------  生成导入模板    负责人：Gary  ------*/
+
+	/*------  保存导入信息    负责人：Gary  ------*/
+
+	/*------  导出人员信息    负责人：无敌霸王龙  ------*/
+	ROUTER_SIMPLE_BIND(ExportPersonController);
+
+	/*------  合同  ------*/
+	/*------  下载模板    负责人：星星果  ------*/
+	ROUTER_SIMPLE_BIND(DownloadEntrustController);
+
+	/*------  保存附件    负责人：y  ------*/
+
+	/*------  附件预览    负责人：kzz  ------*/
 }
 
 #ifdef HTTP_SERVER_DEMO
