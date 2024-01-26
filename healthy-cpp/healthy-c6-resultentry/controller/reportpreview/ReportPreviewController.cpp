96e0725fb6a363a035fcc25f20a878d1b016b792
地灵殿 <12257055+DiLingDian@user.noreply.gitee.com>
2024-01-26 16:44:55 +0800

    基本完成所有内容，PDF部分排版不行后期应该要调整一下，fastdfs没有服务器可以实验，暂时没有写上正确的服务器地址

--- healthy-cpp/healthy-c6-resultentry/controller/reportpreview/ReportPreviewController.cpp
+++ healthy-cpp/healthy-c6-resultentry/controller/reportpreview/ReportPreviewController.cpp
@@ -1,6 +1,24 @@
+#include "stdafx.h"
 #include "ReportPreviewController.h"
+#include"service/reportpreview/ReportPreviewService.h"
+#include"dldlib/pdf/Pdf.h"
+#include"dldlib/fastdfs/FastDfs.h"
 
-StringJsonVO::Wrapper ReportPreviewController::execQueryReportPreview(const ReportPreviewQuery::Wrapper& qdto)
+StringJsonVO::Wrapper ReportPreviewController::execQueryReportPreview(const ReportPreviewQuery::Wrapper& qdto, const PayloadDTO& payload)
 {
-	return StringJsonVO::Wrapper();
+	// 定义一个Service
+	ReportPreviewService service;
+	// 查询数据
+	auto result1 = service.reportview(qdto, payload);
+	auto result2 = service.symptomview(qdto,payload );
+	//调用PDF
+	string filename = "report";
+	BarcodesPdf pdf;
+	pdf.reportTpl(result1, result2, filename);
+	BarcodesFastDfs fdfs;
+	string downpath = fdfs.barcodesDfs(filename);
+	// 响应结果
+	auto jvo = StringJsonVO::createShared();
+	jvo->success(downpath);
+	return jvo;
 }
