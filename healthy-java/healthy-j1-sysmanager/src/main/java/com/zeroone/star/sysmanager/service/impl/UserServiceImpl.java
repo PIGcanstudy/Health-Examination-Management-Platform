c3814158ad18903e84d2d290da920a888eca7d51
sulinffeng <19330789279@163.com>
2024-01-28 12:51:59 +0800

    状态，用户详情

--- healthy-java/healthy-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/impl/UserServiceImpl.java
+++ healthy-java/healthy-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/impl/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.zeroone.star.sysmanager.service.impl;
 
-import com.alibaba.nacos.shaded.org.checkerframework.checker.units.qual.A;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zeroone.star.project.j1.vo.sysmanager.UserDataVO;
 import com.zeroone.star.project.j1.vo.sysmanager.entiy.Role;
 import com.zeroone.star.sysmanager.entity.TUser;
@@ -9,7 +9,6 @@ import com.zeroone.star.sysmanager.mapper.UserMapper;
 import com.zeroone.star.sysmanager.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,16 +21,27 @@ public class UserServiceImpl implements UserService {
     private RoleMapper roleMapper;
     @Override
     public void setUserStatus(Long id, Integer status) {
+        TUser tUser = userMapper.selectById(id);
+        if (tUser != null) {
+            throw new RuntimeException("用户不存在");
+        }
+        if (status == null) {
+            throw new RuntimeException("状态不能为空");
+        }
         userMapper.updateUserStatus(id,status);
     }
 
     @Override
     public UserDataVO getUserData(Long id) {
         TUser tUser = userMapper.selectById(id);
+        if (tUser == null) {
+            throw new RuntimeException("用户不存在");
+        }
         UserDataVO userDataVO = new UserDataVO();
         BeanUtils.copyProperties(tUser,userDataVO);
         List<Role> rolelist= roleMapper.selectRoleByUserId(id);
         userDataVO.setRoleList(rolelist);
         return userDataVO;
     }
+
 }
