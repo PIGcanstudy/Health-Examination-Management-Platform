6d833f0f9c3ae9bda2804ac8f698952eb6a0e63a
qixi <liuqi2467@foxmail.com>
2024-01-29 00:24:46 +0800

    业务逻辑编写

--- healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/ITStopWordService.java
+++ healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/ITStopWordService.java
@@ -1,7 +1,10 @@
 package com.zeroone.star.sysmanager.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j3.stopword.StopWordDTO;
 import com.zeroone.star.project.dto.j3.stopword.UpdateWordDTO;
+import com.zeroone.star.project.query.j3.StopWordQuery;
 import com.zeroone.star.sysmanager.entity.StopWord;
 
 /**
@@ -14,5 +17,18 @@ import com.zeroone.star.sysmanager.entity.StopWord;
  */
 public interface ITStopWordService extends IService<StopWord> {
 
+    /**
+     * 查询禁用词列表
+     * @param query 分页条件
+     * @return 查询结果
+     */
+    PageDTO<StopWordDTO>listpage(StopWordQuery query);
+    /**
+    * 查询禁用词
+     * 根据名称查询禁用词
+     * @param title 待查询的禁用词
+     * @return DTO
+     */
+    StopWordDTO findByTitle(String title);
     int updateWord(UpdateWordDTO updateWord,String token);
 }
--- healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/impl/TStopWordServiceImpl.java
+++ healthy-java/healthy-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/impl/TStopWordServiceImpl.java
@@ -1,16 +1,41 @@
 package com.zeroone.star.sysmanager.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.cpp.SampleDTO;
+import com.zeroone.star.project.dto.j3.stopword.StopWordDTO;
 import com.zeroone.star.project.dto.j3.stopword.UpdateWordDTO;
+import com.zeroone.star.project.query.j3.StopWordQuery;
 import com.zeroone.star.sysmanager.entity.StopWord;
 import com.zeroone.star.sysmanager.mapper.StopWordMapper;
 import com.zeroone.star.sysmanager.service.ITStopWordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.mapstruct.Mapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.management.Query;
+import java.util.Optional;
 
+/**
+ * StopWord对应MapStruct映射接口
+ */
+@Mapper(componentModel = "spring")
+interface  MsStopWordMapper{
+    /**
+    * 查询停用词
+     * 将实体类DO转换为dto
+     * @param stopWord 待转换DO
+     * @return 转换结果
+     */
+    StopWordDTO StopWordToStopWordDTO(StopWord stopWord);
+}
 /**
  * <p>
  * 禁用词 服务实现类
@@ -26,6 +51,20 @@ public class TStopWordServiceImpl extends ServiceImpl<StopWordMapper, StopWord> 
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    MsStopWordMapper msStopWordMapper;
+
+    @Override
+    public PageDTO<StopWordDTO> listpage(StopWordQuery query) {
+        //构建分页条件对象
+        Page<StopWord> page = new Page<>(query.getPageSize(),query.getPageIndex());
+        //构建查询条件
+        QueryWrapper<StopWord> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("title",query.getTitle());
+        //执行查询
+        Page<StopWord> result = baseMapper.selectPage(page,queryWrapper);
+        return PageDTO.create(result,src->msStopWordMapper.StopWordToStopWordDTO(src));
+    }
 
     @Override
     public int updateWord(UpdateWordDTO updateWord,String token) {
@@ -37,5 +76,17 @@ public class TStopWordServiceImpl extends ServiceImpl<StopWordMapper, StopWord> 
         stopWord.setTitle(updateWord.getTitle());
         int count = stopWordMapper.updateById(stopWord);
         return count;
+    }
+    @Override
+    public StopWordDTO findByTitle(String title) {
+        LambdaQueryWrapper<StopWord> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StopWord::getTitle, title);
+        StopWord stopWord = baseMapper.selectOne(wrapper);
+        StopWordDTO stopWordDTO = new StopWordDTO();
+        BeanUtils.copyProperties(stopWord, stopWordDTO);
+        return stopWordDTO;
+//        LambdaQueryWrapper<StopWordDTO> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.eq(StopWordDTO::getTitle, title);
+//        return baseMapper.selectOne(wrapper);
     }
 }
