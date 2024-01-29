818487b6f2d43840a731824371165f0c18aaa4e0
nightbreezerises <3126404715@qq.com>
2024-01-29 20:32:33 +0800

    完善报告打印确认模块

--- healthy-cpp/healthy-c4-reportprint/service/printReport/declarative/printReportApiClient.h
+++ healthy-cpp/healthy-c4-reportprint/service/printReport/declarative/printReportApiClient.h
@@ -17,8 +17,8 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-#ifndef _SAMPLEAPICLIENT_H_
-#define _SAMPLEAPICLIENT_H_
+#ifndef _PRINTREPORTAPICLIENT_H_
+#define _PRINTREPORTAPICLIENT_H_
 
 // 1 导入必须头文件
 #include "oatpp/web/client/ApiClient.hpp"
@@ -27,23 +27,23 @@
 /**
  * 书写示例ApiClient来演示定义声明式服务
  */
-class SampleApiClient : public oatpp::web::client::ApiClient // 2 继承这个类
+class printReportApiClient : public oatpp::web::client::ApiClient // 2 继承这个类
 {
 	// 3 定义ApiClient控制器使用宏---开始
 #include OATPP_CODEGEN_BEGIN(ApiClient)
 	
 	// 4 初始化定义
-	API_CLIENT_INIT(SampleApiClient);
+	API_CLIENT_INIT(printReportApiClient);
 	
 	// 5 使用API_CALL或API_CALL_ASYNC声明调用服务接口
 
 	// 通过ID查询
-	// API_CALL(API_M_GET, "/sample/query-one", queryById, API_HANDLER_AUTN_HEADER, QUERY(UInt64, id));
+	// API_CALL(API_M_GET, "/query-one", queryById, API_HANDLER_AUTN_HEADER, QUERY(UInt64, id));
 	// 多条件查询
-	// API_CALL(API_M_GET, "/sample/query-all", queryAll, API_HANDLER_AUTN_HEADER, API_HANDLER_PAGE_PARAME, QUERY(String, name));
+	// API_CALL(API_M_GET, "/query-all", queryAll, API_HANDLER_AUTN_HEADER, API_HANDLER_PAGE_PARAME, QUERY(String, name));
 	
 #include OATPP_CODEGEN_END(ApiClient)
 	// 3 取消ApiClient控制器使用宏---结束
 };
 
-#endif // !_SAMPLEAPICLIENT_H_
+#endif // !_PRINTREPORTAPICLIENT_H_
