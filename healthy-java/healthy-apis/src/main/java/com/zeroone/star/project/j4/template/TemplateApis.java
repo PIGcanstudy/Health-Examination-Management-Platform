a5b340bb16a9f633991d4f5081bae3cae8c12f05
yry <962217972@qq.com>
2024-01-30 22:48:22 +0800

    增加具体功能实现

--- healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j4/template/TemplateApis.java
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j4/template/TemplateApis.java
@@ -4,9 +4,17 @@ import com.zeroone.star.project.j4.dto.TemplateDto;
 import com.zeroone.star.project.vo.JsonVO;
 import com.zeroone.star.project.vo.ResultStatus;
 
+import java.util.List;
+
 public interface TemplateApis {
 
     public JsonVO<ResultStatus> addTemplate(TemplateDto templateDto);
 
-    public JsonVO<TemplateDto> queryTermDetail(Long termId);
+    public JsonVO<TemplateDto> queryTermDetail(String terplateId) throws Exception;
+
+    public JsonVO<ResultStatus> deleteTemplate(List<String> terplateIds);
+
+    public JsonVO<ResultStatus> updateTemplate(TemplateDto templateDto);
+
+
 }
--- healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/j4/dto/TemplateDto.java
+++ healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/j4/dto/TemplateDto.java
@@ -1,22 +1,18 @@
 package com.zeroone.star.project.j4.dto;
 
-import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
+@TableName("t_template")
 @Data
 public class TemplateDto {
     @ApiModelProperty(value = "主键id", example = "12345")
     private String id;
 
-    @ApiModelProperty(value = "组合项目id", example = "proj_001")
-    private String baseProjectId;
 
     @ApiModelProperty(value = "报告类型", example = "类型A")
     private String reportType;
 
-    @ApiModelProperty(value = "类型", example = "类型B")
-    private String type;
 
     @ApiModelProperty(value = "状态", example = "有效")
     private String status;
