cb103621b55e3f9b621a13061324381ee0e234df
FU-QAQ <fuquanyuo@gmail.com>
2024-01-30 10:57:50 +0800

    merge reportlist

--- healthy-cpp/healthy-c8-inspection-report/controller/reportlist/ReportListController.cpp
+++ healthy-cpp/healthy-c8-inspection-report/controller/reportlist/ReportListController.cpp
@@ -1,6 +1,15 @@
-#include "ReportListController.h"
 #include "stdafx.h"
+#include "ReportListController.h"
+#include "../../service/reportlist/ReportListService.h"
+#include "../ApiDeclarativeServicesHelper.h"
 ReportListPageJsonVO::Wrapper ReportListController::execQueryReportList(const ReportListQuery::Wrapper& query, const PayloadDTO& payload)
 {
-	return {};
+	// 定义一个Service
+	ReportListService service;
+	// 查询数据
+	auto result = service.listAll(query);
+	// 响应结果
+	auto jvo = ReportListPageJsonVO::createShared();
+	jvo->success(result);
+	return jvo;
 }
--- healthy-cpp/healthy-c8-inspection-report/domain/query/reportlist/ReportListQuery.h
+++ healthy-cpp/healthy-c8-inspection-report/domain/query/reportlist/ReportListQuery.h
@@ -17,45 +17,50 @@ class ReportListQuery : public PageQuery
 	// 体检编号
 	DTO_FIELD(String, test_num);
 	DTO_FIELD_INFO(test_num) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.test_num");
+		info->description = ZH_WORDS_GETTER("report-list.field.test_num");
 	}
 	// 体检单位
 	DTO_FIELD(String, physical_unit);
 	DTO_FIELD_INFO(physical_unit) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.physical_unit");
+		info->description = ZH_WORDS_GETTER("report-list.field.physical_unit");
 	}
 	// 身份证号
 	DTO_FIELD(String, id_card);
 	DTO_FIELD_INFO(id_card) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.id_card");
+		info->description = ZH_WORDS_GETTER("report-list.field.id_card");
 	}
 
 	// 体检日期
 	DTO_FIELD(String, check_date);
 	DTO_FIELD_INFO(check_date) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.check_date");
+		info->description = ZH_WORDS_GETTER("report-list.field.check_date");
 	}
 
 	// 审核状态
 	DTO_FIELD(Int32, statu);
 	DTO_FIELD_INFO(statu) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.statu");
+		info->description = ZH_WORDS_GETTER("report-list.field.statu");
 	}
 
 	// 姓名
 	DTO_FIELD(String, person_name);
 	DTO_FIELD_INFO(person_name) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.person_name");
+		info->description = ZH_WORDS_GETTER("report-list.field.person_name");
 	}
 	// 性别
 	DTO_FIELD(String, sex);
 	DTO_FIELD_INFO(sex) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.sex");
+		info->description = ZH_WORDS_GETTER("report-list.field.sex");
 	}
 	// 年龄
 	DTO_FIELD(Int32, age);
 	DTO_FIELD_INFO(age) {
-		info->description = ZH_WORDS_GETTER("reportlist.field.age");
+		info->description = ZH_WORDS_GETTER("report-list.field.age");
+	}
+	// 是否合格
+	DTO_FIELD(String, check_result);
+	DTO_FIELD_INFO(check_result) {
+		info->description = ZH_WORDS_GETTER("report-list.field.check_result");
 	}
 };
 
