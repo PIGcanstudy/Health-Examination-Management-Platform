c54006ee1c5eaeff524a52a9321647763149728c
FU-QAQ <fuquanyuo@gmail.com>
2024-01-30 22:06:47 +0800

    common 的 plannamelist 完成初步实现

--- healthy-cpp/healthy-c8-common/controller/packageslist/PackagesListController.cpp
+++ healthy-cpp/healthy-c8-common/controller/packageslist/PackagesListController.cpp
@@ -6,11 +6,5 @@
 
 PackagesListPageJsonVO::Wrapper PackagesListController::execQueryPackagesList(const String& keywords, const PayloadDTO& payload)
 {
-	// 创建客户端对象
-	API_CLIENT_CREATE(ac, om, PackagesListApiClient, "packageslist-api");
-	// 构建凭证
-	std::string token = PayloadDTO::getTokenPrefix() + payload.getToken();
-	// 返回查询结果
-	//return ac->queryAll(token, query->pageIndex, query->pageSize, URIUtil::urlEncode(query->name))->readBodyToDto<SamplePageJsonVO::Wrapper>(om);
 	return {};
 }
