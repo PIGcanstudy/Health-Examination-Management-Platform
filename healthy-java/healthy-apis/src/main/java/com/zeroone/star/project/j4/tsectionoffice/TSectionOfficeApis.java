93bd3fc48421cde8826bc825ad2f82323cc1b607
ayuan0626 <872221329@qq.com>
2024-01-30 15:28:37 +0800

    加注释

--- healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j4/tsectionoffice/TSectionOfficeApis.java
+++ healthy-java/healthy-apis/src/main/java/com/zeroone/star/project/j4/tsectionoffice/TSectionOfficeApis.java
@@ -17,7 +17,27 @@ import java.util.List;
  * @Description:
  */
 public interface TSectionOfficeApis {
+    /**
+     * @Author: ayuan
+     * @param: [TSectionOfficeDTO]
+     * @return: com.zeroone.star.project.vo.JsonVO<com.zeroone.star.project.vo.ResultStatus>
+     * @Description: 新增科室
+     **/
     JsonVO<ResultStatus> addSectionOffice(@Validated TSectionOfficeDTO TSectionOfficeDTO);
+
+    /**
+     * @Author: ayuan
+     * @param: [TSectionOfficeDTO]
+     * @return: com.zeroone.star.project.vo.JsonVO<com.zeroone.star.project.vo.ResultStatus>
+     * @Description: 修改科室
+     **/
     JsonVO<ResultStatus> modifySectionOffice(@Validated TSectionOfficeDTO TSectionOfficeDTO);
+
+    /**
+     * @Author: ayuan
+     * @param: [ids]
+     * @return: com.zeroone.star.project.vo.JsonVO<com.zeroone.star.project.vo.ResultStatus>
+     * @Description: 删除科室
+     **/
     JsonVO<ResultStatus> removeSectionOffice(@NotNull(message = "List不能为空") List<String> ids);
 }
