09320dbc65a7ed1b6bf9b269b456d98453978aa5
ChenShan <1479970363@qq.com>
2024-01-30 21:58:42 +0800

    测试通过

--- healthy-cpp/healthy-c6-resultentry/service/GroupPersonInfo/GroupPersonInfoService.cpp
+++ healthy-cpp/healthy-c6-resultentry/service/GroupPersonInfo/GroupPersonInfoService.cpp
@@ -10,7 +10,9 @@ GroupPersonInfoDTO::Wrapper GroupPersonInfoService::queryInfo(const GroupPersonI
 	GroupPersonInfoDAO dao;
 	list<GroupPersonInfoDO> result = dao.queryInfo(query);
 	// ½«DO×ª»»³ÉDTO
-	GroupPersonInfoDO sub = result.back();
-	ZO_STAR_DOMAIN_DO_TO_DTO(dto, sub, is_pass, Is_pass, test_num, Test_num, id_card, Id_card, person_name, Person_name, sex, Sex, age, Age, physical_type, Physical_type, sporadic_physical, Sporadic_physical, dept, Dept, create_time, Create_time);
+	for (GroupPersonInfoDO sub : result) {
+		ZO_STAR_DOMAIN_DO_TO_DTO(dto, sub, is_pass, Is_pass, test_num, Test_num, id_card, Id_card, person_name, Person_name, sex, Sex, age, Age, physical_type, Physical_type, sporadic_physical, Sporadic_physical, dept, Dept, create_time, Create_time);
+		return dto;
+	}
 	return dto;
 }
