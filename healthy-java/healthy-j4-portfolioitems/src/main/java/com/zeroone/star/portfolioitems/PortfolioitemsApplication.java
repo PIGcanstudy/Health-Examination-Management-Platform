1c4a939a77fc80a95ca329760526240a6c1974f3
rakkaus <1303277814@qq.com>
2024-01-31 15:18:18 +0800

    增加组合项目创建，删除，更改接口

--- healthy-java/healthy-j4-portfolioitems/src/main/java/com/zeroone/star/portfolioitems/PortfolioitemsApplication.java
+++ healthy-java/healthy-j4-portfolioitems/src/main/java/com/zeroone/star/portfolioitems/PortfolioitemsApplication.java
@@ -1,8 +1,10 @@
 package com.zeroone.star.portfolioitems;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * <p>
@@ -14,6 +16,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author 阿伟学长
  * @version 1.0.0
  */
+@MapperScan("com.zeroone.star.portfolioitems.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PortfolioitemsApplication {
