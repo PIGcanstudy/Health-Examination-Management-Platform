22be02e86e9ce6126f7835b4c86c4195607f9cd0
Zhen hua <2509859469@qq.com>
2024-01-31 12:09:51 +0800

    完成字典数据新增修改功能

--- healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/dto/j3/dictdata/AddDictDataDTO.java
+++ healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/dto/j3/dictdata/AddDictDataDTO.java
@@ -11,14 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddDictDataDTO {
-    @ApiModelProperty(value = "名称",example = "健康体检")
+    @ApiModelProperty(value = "名称",example = "健康体检",required = true)
     private String title;
-    @ApiModelProperty(value = "数据值",example = "健康体检")
+    @ApiModelProperty(value = "数据值",example = "健康体检",required = true)
     private String value;
     @ApiModelProperty(value = "备注",example = "A")
     private String description;
     @ApiModelProperty(value = "排序值",example = "1.00")
     private  java.math.BigDecimal sort_order;
-    @ApiModelProperty(value = "是否启用",example = "0")
-    private Integer status ;
+    @ApiModelProperty(value = "是否启用",example = "0",required = true)
+    private Boolean status ;
+    @ApiModelProperty(value = "对应字典编号",example = "1534030617061232640",required = true)
+    private Long dictId;
 }
