ccf64d4c166361bc7ed5fc11757e975b8875c62d
zqs <2810504435@qq.com>
2024-02-01 18:36:48 +0800

    小改动

--- healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/query/j3/DictData/DictDataQuery.java
+++ healthy-java/healthy-domain/src/main/java/com/zeroone/star/project/query/j3/DictData/DictDataQuery.java
@@ -21,14 +21,12 @@ public class DictDataQuery extends PageQuery {
     /**
      * 数据名称
      */
-    @NotBlank(message = "字典数据名称不能为空")
     @ApiModelProperty(value = "数据名称", example = "mysql")
     private String title;
 
     /**
      * 是否启用 0 启用 1 禁用
      */
-    @NotBlank(message = "状态不能为空，0或1")
     @ApiModelProperty(value = "状态", example = "0")
     private int status;
 }
