a4725086e61ce99ef71345cc827beca308333246
FU-QAQ <fuquanyuo@gmail.com>
2024-02-03 17:25:17 +0800

    rm packagelist dir

--- healthy-cpp/healthy-c8-common/controller/packageslist/PackagesListController.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "PackagesListController.h"
-#include "../ApiDeclarativeServicesHelper.h"
-
-
-PackagesListPageJsonVO::Wrapper PackagesListController::execQueryPackagesList(const String& keywords, const PayloadDTO& payload)
-{
-	return {};
-}
