29218b304a4663765b9f3fae578c8514447bbfb3
晨晨 <2783123915@qq.com>
2024-02-04 15:02:21 +0800

    更新 perdelService.cpp

--- healthy-cpp/healthy-c5-exalanding/service/questatistics/perdelService.cpp
+++ healthy-cpp/healthy-c5-exalanding/service/questatistics/perdelService.cpp
@@ -2,8 +2,8 @@
 #include "perdelService.h"
 #include "../../dao/questatistics/perdelDAO.h"
 
-// 通过id删除体检人员
-bool perdelService::removeData(const string& id)
+// 閫氳繃id鍒犻櫎浣撴浜哄憳
+bool perdelService::removeData(const std::string& id)
 {
 	perdelDAO dao;
 	return dao.deleteByPersonId(id) > 0;
