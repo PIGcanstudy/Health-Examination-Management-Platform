63db0a6af05a750b7f3b478438addf59b80779cb
seven <1473008948@qq.com>
2024-02-05 21:34:33 +0800

    debug-seven

--- healthy-frontend/src/apis/public/index.js
+++ healthy-frontend/src/apis/public/index.js
@@ -2,51 +2,51 @@ import Request from '@/apis/request.js'
 const baseUrl = ''
 
 //获取字典数据名称列表（用于输入表单下拉列表）
-export const queryDictnameListApi = (params) => {
+export const queryDictnameList = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/common/query-dictnamelist')
 }
 
 //获取工种名称列表（分页＋条件）
-export const queryJobnameListApi = (params) => {
+export const queryJobnameList = (params) => {
   return Request.requestForm(Request.GET, baseUrl + '/common/query-jobnamelist', params)
 }
 
 //获取组合项目列表（条件＋分页）
-export const queryPackageProlistApi = (params) => {
+export const queryPackageProlist = (params) => {
   return Request.requestForm(Request.GET, baseUrl + '/common/query-packageprolist', params)
 }
 
 //获取组合项目名称列表（用于输入表单下拉列表）
-export const queryPackagePronamelistApi = (params) => {
+export const queryPackagePronamelist = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/common/query-packagepronamelist')
 }
 
 //获取套餐名称列表（条件+分页）
-export const queryPlannameListApi = (params) => {
+export const queryPlannameList = (params) => {
   return Request.requestForm(Request.GET, baseUrl + '/common/query-plannamelist', params)
 }
 
 //获取套餐组合项目列表
-export const queryPlanProlistApi = (params) => {
+export const queryPlanProlist = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/', params)
 }
 
 //科室名称列表
-export const querySelectionnameListApi = (params) => {
+export const querySelectionnameList = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/common/query-planprolist')
 }
 
 //获取术语名称列表（条件+分页）
-export const queryTermnameListApi = (params) => {
+export const queryTermnameList = (params) => {
   return Request.requestForm(Request.GET, baseUrl + '/common/query-sectionnamelist', params)
 }
 
 //获取类型树
-export const queryTypeTreeApi = (params) => {
+export const queryTypeTree = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/common/query-typetree')
 }
 
 //获取用户名称列表（用于输入表单下拉列表框）
-export const queryUesrnameListApi = (params) => {
+export const queryUesrnameList = (params) => {
   return Request.requestJson(Request.GET, baseUrl + '/common/query-uesrnamelist', params)
 }
--- healthy-frontend/src/stores/public/index.js
+++ healthy-frontend/src/stores/public/index.js
@@ -1,56 +1,56 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import {
-  getDictnameListApi,
-  getJobnameListApi,
-  getPackageProlistApi,
-  getPackagePronamelistApi,
-  getPlannameListApi,
-  getPlanProlistApi,
-  getSelectionnameListApi,
-  getTermnameListApi,
-  getTypeTreeApi,
-  getUesrnameListApi
+  queryDictnameListApi,
+  queryJobnameListApi,
+  queryPackageProlistApi,
+  queryPackagePronamelistApi,
+  queryPlannameListApi,
+  queryPlanProlistApi,
+  querySelectionnameListApi,
+  queryTermnameListApi,
+  queryTypeTreeApi,
+  queryUesrnameListApi
 } from '@/apis/public/index.js'
 
 export const usePublicStore = defineStore('public', () => {
   // 重写方法
   //获取字典数据名称列表（用于输入表单下拉列表）
   const getDictnameList = async (params) => {
-    return await getDictnameListApi(params)
+    return await queryDictnameListApi(params)
   }
   //获取工种名称列表（分页＋条件）
   const getJobnameList = async (params) => {
-    return await getJobnameListApi(params)
+    return await queryJobnameListApi(params)
   }
   //获取组合项目列表（条件＋分页）
   const getPackageProlist = async (params) => {
-    return await getPackageProlistApi(params)
+    return await queryPackageProlistApi(params)
   }
   //获取组合项目名称列表（用于输入表单下拉列表）
   const getPackagePronamelist = async (params) => {
-    return await getPackagePronamelistApi(params)
+    return await queryPackagePronamelistApi(params)
   }
   //获取套餐名称列表（条件+分页）
   const getPlannameList = async (params) => {
-    return await getPlannameListApi(params)
+    return await queryPlannameListApi(params)
   }
   //获取套餐组合项目列表
   const getPlanProlist = async (params) => {
-    return await getPlanProlistApi(params)
+    return await queryPlanProlistApi(params)
   }
   //科室名称列表
   const getSelectionnameList = async (params) => {
-    return await getSelectionnameListApi(params)
+    return await querySelectionnameListApi(params)
   }
   const getTermnameList = async (params) => {
-    return await getTermnameListApi(params)
+    return await queryTermnameListApi(params)
   }
   const getTypeTree = async (params) => {
-    return await getTypeTreeApi(params)
+    return await queryTypeTreeApi(params)
   }
   const getUesrnameList = async (params) => {
-    return await getUesrnameListApi(params)
+    return await queryUesrnameListApi(params)
   }
 
   // 暴露出的数据
